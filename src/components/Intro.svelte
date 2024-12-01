5eee110f7301d04440423bcdd5b0aa981c4795f1
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-12-01 20:42:00 +0000

    Create coming soon page

---
+++ src/components/Intro.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { fade } from 'svelte/transition';
+</script>
+  
+<div 
+    class="overlay"
+    transition:fade={{ duration: 1200 }}
+>
+    <div class="content">
+        <h1>Coming soon</h1>
+    </div>
+</div>
+  
+<style>
+    .overlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background-color: #1e252d;
+      color: white;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 2rem;
+      padding: 2rem;
+      box-sizing: border-box;
+      z-index: 9999;
+    }
+  
+    .content {
+      transform: translateY(33%);
+      text-align: center;
+    }
+
+    h1 {
+        font-size: 5rem;
+    }
+
+    @media screen and (max-width: 736px) {
+        h1 {
+            font-size: 3.25rem;
+            line-height: 1.1;
+            margin-bottom: 1rem;
+        }
+    }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$components: path.resolve("./src/components"),
+			$content: path.resolve("./src/content"),
+			$data: path.resolve("./src/data"),
+			$routes: path.resolve("./src/routes"),
+			$styles: path.resolve("./src/styles")
+		}
+	}
 });
