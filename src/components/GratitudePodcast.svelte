ce697ba8e465c4ceab7c69af96087bd7b659c427
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-12-15 15:07:21 +0000

    Add MP3 player

---
+++ src/components/GratitudePodcast.svelte
@@ -0,0 +1,16 @@
+<script>
+    import src from "$lib/assets/gratitude.mp3"
+</script>
+
+<div class="audio-container">
+    <audio controls="" style="height: 50px">
+        <source {src} type="audio/mpeg">    
+        Your browser does not support the audio element.
+    </audio>
+</div>
+
+<style>
+    .audio-container {
+        margin-bottom: 1.5rem;
+    }
+</style>
